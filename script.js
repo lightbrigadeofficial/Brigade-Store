834a13eb7c00ca5409119dd0c1f7bdb41ae22fd0
LightBrigade <lightbrigadeofficial@gmail.com>
2025-02-03 03:05:30 +0530

    v1 commit
    v1 -brigadevandam

---
+++ script.js
@@ -0,0 +1,26 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+})
+const searchButton = document.getElementById('searchButton');
+const searchBar = document.getElementById('searchBar');
+
+searchButton.addEventListener('click', () => {
+    if (!searchBar.classList.contains('visible')) {
+        searchBar.classList.add('visible');
+        searchBar.classList.remove('hidden');
+        searchBar.focus();
+    } else {
+        const query = searchBar.value.trim();
+        if (query) {
+            // Redirect to the results page with the query as a parameter
+            window.location.href = `search-results.html?q=${encodeURIComponent(query)}`;
+        }
+    }
+});
+// Function to toggle the side panel
+function toggleSidePanel() {
+    const sidePanel = document.getElementById('side-panel');
+    sidePanel.classList.toggle('open');
+}
